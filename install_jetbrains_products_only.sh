11c218e708185175afcc3bb8a642f06bdf2fca38
Tualik <aram.ds@gmail.com>
2016-06-21 17:04:48 +0400

    corrections

--- install_jetbrains_products_only.sh
+++ install_jetbrains_products_only.sh
@@ -34,47 +34,50 @@ read type
 if [ "$type" == "n" ]; then
   exit 0
 fi
+#
+#code=`lsb_release -a | grep Codename | sed 's/[[:space:]]//g' | cut -f2 -d:`
+#
+#echo
+#echo "Debian codename:"
+#echo "$code"
+#echo
+#
+#mkdir -p /var/tmp
+#pushd /var/tmp
+#
+#echo
+#echo "Installing Java JDK 1.8"
+#echo
+#
+#if [ "$code" != "jessie" ]; then
+#  echo "from oracle site"
+#  echo
+#  url=http://download.oracle.com/otn-pub/java/jdk/8u91-b14/
+#  java_version=jdk-8u91-linux-x64.tar.gz
+#
+#  wget -c -O "$java_version" --no-check-certificate --no-cookies --header "Cookie: oraclelicense=accept-securebackup-cookie" "$url$java_version"
+#
+#  mkdir -p /opt/jdk
+#
+#  tar -zxf ${java_version} -C /opt/jdk
+#
+#  update-alternatives --install /usr/bin/java java /opt/jdk/jdk1.8.0_91/bin/java 100
+#  update-alternatives --install /usr/bin/javac javac /opt/jdk/jdk1.8.0_91/bin/javac 100
+#else
+#  apt-get install java8-jdk -y
+#fi;
+#
+#echo
+#java -version
+#update-alternatives --display java
+#javac -version
+#update-alternatives --display javac
+#echo
 
-code=`lsb_release -a | grep Codename | sed 's/[[:space:]]//g' | cut -f2 -d:`
-
-echo
-echo "Debian codename:"
-echo "$code"
-echo
-
-mkdir -p /var/tmp
-pushd /var/tmp
-
-echo
-echo "Installing Java JDK 1.8"
-echo
-
-if [ "$code" != "jessie" ]; then
-  echo "from oracle site"
-  echo
-  url=http://download.oracle.com/otn-pub/java/jdk/8u91-b14/
-  java_version=jdk-8u91-linux-x64.tar.gz
-
-  wget -c -O "$java_version" --no-check-certificate --no-cookies --header "Cookie: oraclelicense=accept-securebackup-cookie" "$url$java_version"
-
-  mkdir -p /opt/jdk
-
-  tar -zxf ${java_version} -C /opt/jdk
-
-  update-alternatives --install /usr/bin/java java /opt/jdk/jdk1.8.0_91/bin/java 100
-  update-alternatives --install /usr/bin/javac javac /opt/jdk/jdk1.8.0_91/bin/javac 100
-else
-  apt-get install java8-jdk -y
-fi;
-
-echo
-java -version
-update-alternatives --display java
-javac -version
-update-alternatives --display javac
-echo
-
-mkdir -p /usr/jetbrains/{youtrack, hub, upsource, teamcity}
+mkdir -p /usr/jetbrains/youtrack
+mkdir -p /usr/jetbrains/hub
+mkdir -p /usr/jetbrains/upsource
+mkdir -p /usr/jetbrains/teamcity
 
 wget http://download.jetbrains.com/hub/2.0/hub-ring-bundle-2.0.244.zip -O /usr/jetbrains/hub/arch.zip
 
